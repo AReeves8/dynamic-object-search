7ae09e1a54783a05b09c59099171a0ba52a151d2
AReeves8 <areeves@skillstorm.com>
2024-03-20 21:05:54 -0500

    finished component communication and styling

--- force-app/main/default/lwc/fieldSelector/fieldSelector.js
+++ force-app/main/default/lwc/fieldSelector/fieldSelector.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
 import getObjectFields from '@salesforce/apex/DynamicObjectSearchController.getObjectFields'
+import { MessageContext, publish } from 'lightning/messageService'
+import OBJECT_SEARCH_CHANNEL from '@salesforce/messageChannel/ObjectSearch__c';
 
 export default class FieldSelector extends LightningElement {
 
@@ -8,6 +10,11 @@ export default class FieldSelector extends LightningElement {
     allFieldsList = [];             // all fields from the sObject
     selectedFieldsList = [];        // all fields that have been selected by the user
     errorMessage;                   // text to display if any errors occur
+    disableButton = true;           // toggle for submit button
+
+    @wire(MessageContext)
+    messageContext;                 // wiring in Lightning Message Service
+
 
     // calling an apex method to retrieve all the fields for the provided object
     @wire(getObjectFields, {objectName : '$objectToUse'})       // $ makes this action reactive to every change made to objectToUse
@@ -28,15 +35,40 @@ export default class FieldSelector extends LightningElement {
         }
     }
 
+    // event handler for when fields are either selected or removed from the selected list
     handleSelections(event) {
+
+        // set the fields
         this.selectedFieldsList = event.detail.value;
+        
+        // toggle button on/off
+        if(this.selectedFieldsList.length > 0) {
+            this.disableButton = false;
+        }
+        else {
+            this.disableButton = true;
+        }
     }
 
+    // broadcast new event when submission occurs
     handleSubmit(event) {
+
+        const payload = {
+            object : this.objectToUse,
+            fields : this.selectedFieldsList
+        }
+
+        publish(this.messageContext, OBJECT_SEARCH_CHANNEL, payload);
+        
+        /*
+
+        This is what you would do to send data back up to the parent objectSelector component
+
         const fieldsSubmitted = new CustomEvent('fieldsubmit', {
-            detail : this.selectedFieldsList
+            detail: this.selectedFieldsList
         });
 
         this.dispatchEvent(fieldsSubmitted);
+        */
     }
 }
--- force-app/main/default/lwc/objectSelector/objectSelector.js
+++ force-app/main/default/lwc/objectSelector/objectSelector.js
@@ -3,22 +3,19 @@ import { LightningElement } from 'lwc';
 
 export default class ObjectSelector extends LightningElement {
 
-
-    objectSelected = false;
-    selectedObject;
-    objectsList = [
+    objectSelected = false;                             // toggle for showing field options
+    selectedObject;                                     // the object selected by the user
+    objectsList = [                                     // the available object options - formatted for use in lightning-combobox
         { label : 'Account', value : 'Account'},
         { label : 'Contact', value : 'Contact'},
         { label : 'Lead', value : 'Lead'}
     ];
 
+
+    // handling when an object is selected from the drop-down
     handleSelection(event) {
         this.objectSelected = true;
         this.selectedObject = event.detail.value;
     }
 
-    handleFields(event) {
-        const fieldsList = event.detail;
-        
-    }
 }
